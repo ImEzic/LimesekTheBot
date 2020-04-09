62ba22ab7f3e1390d18d6116e73b8071d49f0502
Ezic <shutdown.ssh@gmail.com>
2020-04-09 18:19:28 +0200

    Added 8ball

--- cogs/fun.py
+++ cogs/fun.py
@@ -48,7 +48,30 @@ class Fun(commands.Cog):
             await message.channel.send(random.choice(wittyResponse))
         
 
-
+    #*8ball
+    @commands.command():
+    async def 8ball(self, ctx, message):
+        magicResponse = ['It is certain.',
+                        'It is decidedly so.',
+                        'Without a doubt.',
+                        'Yes - definitely.',
+                        'You may rely on it.',
+                        'As I see it, yes.',
+                        'Most likely.',
+                        'Outlook good.',
+                        'Yes.',
+                        'Signs point to yes.',
+                        'Reply hazy, try again.',
+                        'Ask again later.',
+                        'Better not tell you now.',
+                        'Cannot predict now.',
+                        'Concentrate and ask again.',
+                        'Don\'t count on it.',
+                        'My reply is no.',
+                        'My sources say no.',
+                        'Outlook not so good.',
+                        'Very doubtful.']
+        await ctx.send(random.choice(magicResponse))
 
     #*Let me google that for you
     @commands.command()
