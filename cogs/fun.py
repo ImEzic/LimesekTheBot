b936a930815625ba207ad96e4714b250f8ed9fa6
Ezic <shutdown.ssh@gmail.com>
2020-04-09 18:33:18 +0200

    Added 8ball

--- cogs/fun.py
+++ cogs/fun.py
@@ -50,27 +50,27 @@ class Fun(commands.Cog):
 
     #*8ball
     @commands.command()
-    async def 8ball(self, ctx, message):
+    async def 8ball(self, ctx, *,message):
         magicResponse = ['It is certain.',
-                        'It is decidedly so.',
-                        'Without a doubt.',
-                        'Yes - definitely.',
-                        'You may rely on it.',
-                        'As I see it, yes.',
-                        'Most likely.',
-                        'Outlook good.',
-                        'Yes.',
-                        'Signs point to yes.',
-                        'Reply hazy, try again.',
-                        'Ask again later.',
-                        'Better not tell you now.',
-                        'Cannot predict now.',
-                        'Concentrate and ask again.',
-                        'Don\'t count on it.',
-                        'My reply is no.',
-                        'My sources say no.',
-                        'Outlook not so good.',
-                        'Very doubtful.']
+                'It is decidedly so.',
+                'Without a doubt.',
+                'Yes - definitely.',
+                'You may rely on it.',
+                'As I see it, yes.',
+                'Most likely.',
+                'Outlook good.',
+                'Yes.',
+                'Signs point to yes.',
+                'Reply hazy, try again.',
+                'Ask again later.',
+                'Better not tell you now.',
+                'Cannot predict now.',
+                'Concentrate and ask again.',
+                'Don\'t count on it.',
+                'My reply is no.',
+                'My sources say no.',
+                'Outlook not so good.',
+                'Very doubtful.']
         await message.channel.send(random.choice(magicResponse))
 
     #*Let me google that for you
