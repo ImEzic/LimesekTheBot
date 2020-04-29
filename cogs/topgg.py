fe2d230f8c64e13cb5eb21813e9846f0e1eb5eed
Ezic <shutdown.ssh@gmail.com>
2020-04-29 22:18:51 +0200

    adding server count

--- cogs/topgg.py
+++ cogs/topgg.py
@@ -8,7 +8,7 @@ class TopGG(commands.Cog):
 
     def __init__(self, client):
         self.client = client
-        self.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4ODM5NzA5MjcwNzYzMTEyNSIsImJvdCI6dHJ1ZSwiaWF0IjoxNTg3ODQzNzgyfQ.gpLYaqZicN4mozzdygrUeEV77WeSZXZR3_XqzhNHZKk"
+        self.token = os.environ.get("TOPGG_TOKEN")
         self.dblpy = dbl.DBLClient(self.client, self.token, autopost=True)
 
     async def on_guild_post(self):
@@ -16,3 +16,4 @@ class TopGG(commands.Cog):
 
 def setup(client):
     client.add_cog(TopGG(client))
+    
