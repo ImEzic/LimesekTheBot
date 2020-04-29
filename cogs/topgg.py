5badddd8ce79c9c1a5c767729ec9ad901ab6861b
Ezic <shutdown.ssh@gmail.com>
2020-04-29 21:17:34 +0200

    adding server count

---
+++ cogs/topgg.py
@@ -0,0 +1,19 @@
+import os
+import dbl
+import discord
+from discord.ext import commands
+
+
+class TopGG(commands.Cog):
+    """Handles interactions with the top.gg API"""
+
+    def __init__(self, client):
+        self.client = client
+        self.token = os.environ.get("TOPGG_TOKEN") 
+        self.dblpy = dbl.DBLClient(self.client, self.token, autopost=True) # Autopost will post your guild count every 30 minutes
+
+    async def on_guild_post(self):
+        print("Server count posted successfully")
+
+def setup(client):
+    client.add_cog(TopGG(client))
