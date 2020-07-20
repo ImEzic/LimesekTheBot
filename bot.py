8dccfc79d30b64f59f7afdb5c4a78e4104cbdb32
Ezic <shutdown.ssh@gmail.com>
2020-07-20 12:19:39 +0200

    top.gg server count

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import discord
 import os
 from discord.ext import commands
 import requests
+import time
 
 __version__ = '1.0.0'
 
@@ -39,13 +40,15 @@ async def on_ready():
     "authorization": os.environ.get("TOPGG_TOKEN"),
     "user-agent": "Discord-Bot-That-Does-Stuff/1.1 Python/3.8 requests/2.23.0"
     }
-    payload = {"server_count": len(client.guilds)} # The amount you want to post (dont falsify this)
-    req = requests.post("https://top.gg/api/bots/688397092707631125/stats", json=payload, headers=headers)
-    if 199 < req.status_code < 300:
-        print("Successfully posted '" + str(payload) + "' to TopGG.")
-    else:
-        print("Failed to post guild count to TopGG, response code {}".format(req.status_code))
-    
+    while True:
+        payload = {"server_count": len(client.guilds)} # The amount you want to post (dont falsify this)
+        req = requests.post("https://top.gg/api/bots/688397092707631125/stats", json=payload, headers=headers)
+        if 199 < req.status_code < 300:
+            print("Successfully posted '" + str(payload) + "' to TopGG.")
+        else:
+            print("Failed to post guild count to TopGG, response code {}".format(req.status_code))
+        time.sleep(60)
+
     #* Bot Status
     await client.change_presence(status=discord.Status.online, activity=discord.Game('Making Pancakes'))
 
