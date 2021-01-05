4eb11d92760cbb77adfbd47d0ae4429fb006e38a
Ezic <shutdown.ssh@gmail.com>
2021-01-05 11:14:31 +0100

    updates

--- bot.py
+++ bot.py
@@ -37,24 +37,24 @@ async def on_ready():
     await client.change_presence(status=discord.Status.online, activity=discord.Game('Making Pancakes'))
     
     
-#*TopGG Server count
-@commands.command()
-@commands.is_owner()
-async def topgg(ctx):
-    headers = {  
-        "content-type": "application/json",
-        "authorization": os.environ.get("TOPGG_TOKEN"),
-        "user-agent": "Discord-Bot-That-Does-Stuff/1.0.0 Python/3.8.2 requests/2.23.0"
-    }
-    while True:
-        payload = {"server_count": len(client.guilds)}
-        req = requests.post("https://top.gg/api/bots/688397092707631125/stats".format(str(client.user.id)), json=payload, headers=headers)
-        if 199 < req.status_code < 300:
-            print("Successfully posted '" + str(payload) + "' to TopGG.")
-        else:
-            print("Failed to post guild count to TopGG, response code {}".format(req.status_code))
-            break
-        time.sleep(300) # change that to 3600
+# #*TopGG Server count
+# @commands.command()
+# @commands.is_owner()
+# async def topgg(ctx):
+#     headers = {  
+#         "content-type": "application/json",
+#         "authorization": os.environ.get("TOPGG_TOKEN"),
+#         "user-agent": "Discord-Bot-That-Does-Stuff/1.0.0 Python/3.8.2 requests/2.23.0"
+#     }
+#     while True:
+#         payload = {"server_count": len(client.guilds)}
+#         req = requests.post("https://https://top.gg/api/bots/688397092707631125/stats".format(str(client.user.id)), json=payload, headers=headers)
+#         if 199 < req.status_code < 300:
+#             print("Successfully posted '" + str(payload) + "' to TopGG.")
+#         else:
+#             print("Failed to post guild count to TopGG, response code {}".format(req.status_code))
+#             break
+#         time.sleep(300) # change that to 3600
 
 
 @client.event
